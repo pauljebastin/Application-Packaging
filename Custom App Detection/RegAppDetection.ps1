43ee8b9acf73bac1ad65d34b56c33a21b27fda12
Paul Jebastin A. <jebastin.paul@gmail.com>
2026-05-15 15:22:28 +0100

    Create RegAppDetection.ps1
    Adding Application Detection using Registery Key

---
+++ Custom App Detection/RegAppDetection.ps1
@@ -0,0 +1,36 @@
+﻿# #################################################
+# Application Detection using Registery Key       #
+# No dependency on ConfigMgr client               #
+# Works on Intune-only devices                    #
+# Fast and lightweight                            #
+# Proper Intune detection exit codes              #
+# #################################################
+
+$AppName = '<App_Name>'
+$MinVersion = [version]"App_Version"
+
+
+$Paths = @(
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
+    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+)
+
+$App = Get-ItemProperty $Paths -ErrorAction SilentlyContinue | Where-Object {$_.DisplayName -like "*$($AppName)*" } | Select-Object -First 1
+
+if($App){
+    try {
+        $InstalledVersion = [version]$App.DisplayVersion
+
+        if ($InstalledVersion -ge $MinVersion) {
+            Write-Output "Installed"
+            exit 0
+        }
+    }
+    catch{
+        #Write-Output "App Detection Error"
+        exit 1
+    }
+}else{
+    Write-Output "Not Installed"
+    exit 1
+}
