4fc3e310fd0ee01ea174914c78336f27b03c0aa0
Paul Jebastin A. <jebastin.paul@gmail.com>
2026-05-15 15:37:28 +0100

    Create ConfigMgrAppDetection.ps1
    Adding Application Detection using Config Manager WMi table

---
+++ Custom App Detection/ConfigMgrAppDetection.ps1
@@ -0,0 +1,21 @@
+﻿# #################################################
+# Application Detection using Config Manager WMi  #
+# ConfigMgr client Dependent                      #
+# Works only with ConfigMgr deployed apps         #
+# Fast and lightweight                            #
+# #################################################
+
+$AppName = '<App_Name>'
+$MinVersion = [version]"App_Version"
+
+$AppInstall = Get-WmiObject -Namespace "root\cimv2\sms" -Class "SMS_InstalledSoftware" | Where-Object ProductName -Match "$($AppName)"
+
+if($AppInstall){
+    if([version]$AppInstall.ProductVersion -ge $MinVersion){
+        Write-Host "Installed"
+        exit 0
+    }
+}
+
+Write-Host "Not Installed"
+exit 1
