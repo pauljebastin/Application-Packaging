d0d28b8180d12eb9fc5126a08c09140ec8ed152c
Paul Jebastin A. <jebastin.paul@gmail.com>
2026-05-15 15:28:52 +0100

    Create FileAppDetection.ps1
    Adding Application Detection using Application File

---
+++ Custom App Detection/FileAppDetection.ps1
@@ -0,0 +1,24 @@
+﻿# ############################################### #
+# Application Detection using Application File    #
+# No dependency on ConfigMgr client               #
+# More relliable than uninstall reg key           #
+# Fast and lightweight                            #
+# Proper Intune detection exit codes              #
+# ############################################### #
+
+$Application = "C:\Program Files\ApplicationFolder\<ApplicationFile>.exe"
+$MinVersion = [version]"<App_Version>"
+
+if (Test-Path $Application) {
+
+    $Version = [version](Get-Item $Application).VersionInfo.FileVersion
+
+    if ($Version -ge $MinVersion) {
+        Write-Output "Installed"
+        exit 0
+    }
+}else{
+    Write-Output "Not Installed"
+    exit 1
+}
+
